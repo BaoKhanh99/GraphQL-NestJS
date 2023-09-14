3e9edaa4aef4e578cb858a54f460ea51ab87bb33
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2023-09-14 17:02:46 +0700

    config graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseProviderModule } from './database/database-provider.module';
+import { GraphQLProviderModule } from './graphql/graphql.module';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule],
+  imports: [
+    AppConfigModule,
+    DatabaseProviderModule,
+    GraphQLProviderModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
